a4d8f8b9273cf138fcdac19d71ba9f295a8acf70
iAsure <asuregames01@gmail.com>
2023-10-31 14:45:59 -0600

    csurf instalation

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,7 @@ async function bootstrap() {
       'Content-Type, Accept, Authorization, Access-Control-Allow-Origin, x-apollo-operation-name, apollo-require-preflight',
     credentials: true,
   });
+  app.use(csurf());
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
