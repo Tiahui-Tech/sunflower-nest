a867eb59ebb27c3069bb55329c7a8584198d5eb2
iAsure <asuregames01@gmail.com>
2023-10-31 11:55:33 -0600

    api cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,POST',
+  });
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
 }
