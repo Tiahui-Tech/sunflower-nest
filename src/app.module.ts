8dfef3d34f7fac03211476d18498d7b0d718a8ad
iAsure <asuregames01@gmail.com>
2023-10-31 12:22:04 -0600

    csrf disabled

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { AnimeModule } from './anime/anime.module';
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      stopOnTerminationSignals: false,
     }),
     UserModule,
     RecommendationModule,
