b6522aaca15f8435203e86e7f4c7d6bc0a783f83
iAsure <asuregames01@gmail.com>
2023-10-31 14:55:05 -0600

    uninstalled csurf

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,7 +11,6 @@ async function bootstrap() {
       'Content-Type, Accept, Authorization, Access-Control-Allow-Origin, x-apollo-operation-name, apollo-require-preflight',
     credentials: true,
   });
-  app.use(csurf());
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
